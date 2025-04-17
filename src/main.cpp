d4a46567705a76a9d4cd730fbedeceabf194cffb
Shokhruz Azzamkulov <azzamkulovshokhruz@gmail.com>
2025-04-17 16:38:17 +0500

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <SFML/Graphics.hpp>
+#include <array>
+#include <string>
+#include <utility>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+using namespace sf;
+
+struct Point {
+private:
+	Vertex& vertex;
+
+public:
+
+	Point(Vertex& _v) : vertex(_v) {}
+
+	Vertex& getVertex() {
+		return vertex;
+	}
+
+};
+
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "Graph Tree");
+	window.setFramerateLimit(15);
+
+}
